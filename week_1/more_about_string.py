fc23208bfe5549a6efca4f290bdec85753be2c2e
omkumar2 <omkumariitbomboy@gmail.com>
2025-02-19 00:05:43 +0530

    add blank line for improved readability in more_about_string.py

--- week_1/more_about_string.py
+++ week_1/more_about_string.py
@@ -1,15 +1,19 @@
 s="good"
 print(s[0]*5) #ggggg
+
 #next
 x="india"
 print(x== "india") #true
 print(x== "India") #false
+
 #next
 print("apple" > "one" ) #false
 print( "four" < "ten") #true
 print( 4 < 10) #true
 print("ab" < "az") #true
 print("abcdef" < "abcde") #true
+print(ord('d'))  # Output: 97  ,  this is the ascii value of d
+
 #next
 s="python"
 print(s[-1]) #n
@@ -18,6 +22,7 @@ print(s[-3]) #h
 print(s[-4]) #t
 print(s[-5]) #y
 print(s[-6]) #p
+
 #next
 s="hkgjhkgkkgjkjdghfuu"
 print(len(s)) #19  = this is number of letter in line
