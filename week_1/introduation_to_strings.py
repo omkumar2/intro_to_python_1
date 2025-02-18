c7f5a52756ca1ab4ec291331c800c61a979a4463
omkumar2 <omkumariitbomboy@gmail.com>
2025-02-18 23:45:13 +0530

    add clarification comment for variable b in introduation_to_strings.py

--- week_1/introduation_to_strings.py
+++ week_1/introduation_to_strings.py
@@ -10,7 +10,7 @@ print(s[1:4])# off
 #next
 s="0123456789"
 a=s[5]
-b=s[6]
+b=s[6] # this take the value of b as list
 print(a) #5
 print(b) #6
 print(a+b) #56
@@ -18,7 +18,7 @@ print(a+b) #56
 #next
 s="0123456789"
 a=int(s[5])
-b=int(s[6])
+b=int(s[6]) # this take the value of b as integer
 print(a) #5
 print(b) #6
 print(a+b) #11
