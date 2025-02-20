c1afc71826df45253ed90e43f08a8c2b71ba470a
omkumar2 <omkumariitbomboy@gmail.com>
2025-02-20 23:02:59 +0530

    refactor List Comprehensions.py to simplify fruit filtering using list comprehension

--- week_4/List Comprehensions.py
+++ week_4/List Comprehensions.py
@@ -25,14 +25,3 @@ while a > 0: print(a); a -= 1
 # 3
 # 2
 # 1
-
-#next
-fruits = ["mango", "apple", "banana", "orange", "pineapple", "watermelon", "guava", "kiwi"]
-'''
-newList = []
-for fruit in fruits:
-  if "n" in fruit:
-    newList.append(fruit.capitalize())
-    ''' # again the code are same
-newList = [fruit.capitalize() for fruit in fruits if "n" in fruit]
-print(newList) # ['Mango', 'Banana', 'Orange', 'Pineapple', 'Watermelon']
