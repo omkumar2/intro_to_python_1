3887f2137ffbb8e4365b7ff44f58f79f625a2b57
omkumar2 <omkumariitbomboy@gmail.com>
2025-05-07 18:56:39 +0530

    feat: add Jupyter notebook for v shape printing functionality

--- google_ai.py
+++ google_ai.py
@@ -1,7 +1,7 @@
 
 from google import genai
 
-client = genai.Client(api_key="")
+client = genai.Client(api_key="AIzaSyDOjRDvAfF_QWKMppG3pWdqIcTJPGdTeKw")
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents=input()
