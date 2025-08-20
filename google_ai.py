75fef14433b02b7709dfa950e1fee06732761a63
omkumar2 <omkumariitbomboy@gmail.com>
2025-08-20 17:40:52 +0530

    solution of z-test

--- google_ai.py
+++ google_ai.py
@@ -10,3 +10,10 @@ response: ChatResponse = chat(model='gemma3:latest', messages=[
 print(response['message']['content'])
 
 
+
+
+from scipy import stats
+
+n = 0.95
+result = stats.norm.ppf(n) * (3/4) + 9.5
+print(result)
