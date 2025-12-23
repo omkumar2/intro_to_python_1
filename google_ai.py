664dc07ecbda072b67060a3bf89f14ca14fe6774
omkumar2 <omkumariitbomboy@gmail.com>
2025-12-23 20:40:52 +0530

    feat: update package version to 3.13.9 across multiple notebooks and remove unused code from google_ai.py and om.py

--- google_ai.py
+++ google_ai.py
@@ -1,7 +1,7 @@
 from ollama import chat
 from ollama import ChatResponse
 
-response: ChatResponse = chat(model='gemma3:latest', messages=[
+response: ChatResponse = chat(model='', messages=[
   {
     'role': 'assistant',
     'content': input("Enter your message: "),
@@ -12,19 +12,5 @@ print(response['message']['content'])
 
 
 
-from scipy import stats
-
-n = 0.95
-result = stats.norm.ppf(n) * (3/4) + 9.5
-print(result)
 
 
-print("This is a test message to check the functionality of the code.")
-import numpy as np
-import matplotlib.pyplot as plt
-x = np.linspace(-10, 10, 100)
-y = np.sin(x) 
-plt.plot(x, y)
-plt.title('Sine Wave')
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
